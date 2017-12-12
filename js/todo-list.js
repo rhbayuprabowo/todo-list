4efcee2a7566e94b04cfe4eb3d4420c8fef7d77d
R. H. Bayu Prabowo <rh.bayu.prabowo@outlook.com>
2017-12-12 12:15:38 +0700

    complete base code

--- js/todo-list.js
+++ js/todo-list.js
@@ -1,7 +1,18 @@
-$("li").on("click", function() {
-	$(this).toggleClass("completed");
+$('ul').on('click', 'li', function() {
+  $(this).toggleClass('completed');
 });
 
-$("span").on("click", function() {
-	alert("test");
+$('ul').on('click', 'span', function(e) {
+  $(this).parent().fadeOut(500, function() {
+    $(this).remove();
+  });
+  e.stopPropagation();
+});
+
+$('input[type=\'text\']').keypress(function(e) {
+  if (e.which === 13) {
+    var value = $(this).val();
+    $(this).val('');
+    $('ul').append('<li><span>X</span> ' + value + '</li>');
+  }
 });
