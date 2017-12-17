4f09ca8d22afc3f9814eef1befe9f839091fd4d6
R. H. Bayu Prabowo <rh.bayu.prabowo@outlook.com>
2017-12-17 12:27:29 +0700

    "" to ''

--- js/todo-list.js
+++ js/todo-list.js
@@ -1,22 +1,23 @@
-$('ul').on('click', 'li', function() {
-  $(this).toggleClass('completed');
-});
-
-$('ul').on('click', 'span', function(e) {
-  $(this).parent().fadeOut(500, function() {
-    $(this).remove();
+$('ul').on('click', 'li', function () {
+    $(this).toggleClass('completed');
   });
-  e.stopPropagation();
-});
+
+$('ul').on('click', 'span', function (e) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+      });
+
+    e.stopPropagation();
+  });
 
 $('input[type=\'text\']').keypress(function (e) {
-  if (e.which === 13) {
-    var value = $(this).val();
-    $(this).val('');
-    $('ul').append('<li><span><i class="fa fa-trash"></i></span> ' + value + '</li>');
-  }
-});
+    if (e.which === 13) {
+      var value = $(this).val();
+      $(this).val('');
+      $('ul').append('<li><span><i class="fa fa-trash"></i></span> ' + value + '</li>');
+    }
+  });
 
-$(".fa-sticky-note").on("click", function () {
-  $('input[type =\'text\']').slideToggle();
-});
+$('.fa-sticky-note').on('click', function () {
+    $('input[type =\'text\']').slideToggle();
+  });
